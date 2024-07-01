5588315bb8043a0e8d513c2d5c4a99f8f193906d
Victtor777 <victtor.eng.software@gmail.com>
2024-07-01 18:01:55 -0300

    Finalizando os estilos da página Home

--- src/components/PopularBooks/index.tsx
+++ src/components/PopularBooks/index.tsx
@@ -1,3 +1,4 @@
+import { BookCard } from "../BookCard";
 import { Text } from "../Typography";
 import { Link } from "../ui/Link";
 import { Container } from "./styles";
@@ -9,6 +10,25 @@ export const PopularBooks = () => {
         <Text size={"sm"}>Livros populares</Text>
         <Link href={"/explore"} text="Ver todos" />
       </header>
+
+      <section>
+        {Array.from({ length: 4 }).map((_, i) => (
+          <BookCard
+            key={`popular-${i}`}
+            book={{
+              author: "John Doe",
+              avgRating: 4,
+              cover_url: "https://github.com/Victtor-777.png",
+              created_at: new Date(),
+              id: "sajksajk72382y38",
+              name: "Lorem Ipsum color sit amet",
+              summary:
+                "Lorem Ipsum color sit amet, Lorem Ipsum color sit amet, Lorem Ipsum color sit amet",
+              total_pages: 100,
+            }}
+          />
+        ))}
+      </section>
     </Container>
   );
 };
