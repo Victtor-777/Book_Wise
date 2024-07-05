8abac0d099d7069067fb5c33b0c615f49148f117
Victtor777 <victtor.eng.software@gmail.com>
2024-07-05 18:30:21 -0300

    Add base de estilos do livro no modal

--- src/components/RatingsDialog/index.tsx
+++ src/components/RatingsDialog/index.tsx
@@ -1,18 +1,28 @@
-import * as Dialog from "@radix-ui/react-dialog"
-import { ReactNode } from "react"
-import { DialogClose, DialogContent, DialogOverlay } from "./styles"
-import { X } from "@phosphor-icons/react"
+import * as Dialog from "@radix-ui/react-dialog";
+import { ReactNode } from "react";
+import {
+  BookContent,
+  BookDetailsContainer,
+  BookDetailsWrapper,
+  BookImage,
+  BookInfos,
+  DialogClose,
+  DialogContent,
+  DialogOverlay,
+} from "./styles";
+import { BookmarkSimple, BookOpen, X } from "@phosphor-icons/react";
+import { Heading, Text } from "../Typography";
+import { RatingStars } from "../RatingStars";
+import { BookInfo } from "./BookInfo";
 
 type RatingsDialogProps = {
-  children: ReactNode
-}
+  children: ReactNode;
+};
 
 export const RatingsDialog = ({ children }: RatingsDialogProps) => {
   return (
     <Dialog.Root>
-      <Dialog.Trigger asChild>
-        {children}
-      </Dialog.Trigger>
+      <Dialog.Trigger asChild>{children}</Dialog.Trigger>
 
       <Dialog.Portal>
         <DialogOverlay />
@@ -21,8 +31,40 @@ export const RatingsDialog = ({ children }: RatingsDialogProps) => {
           <DialogClose>
             <X size={24} />
           </DialogClose>
+
+          <BookDetailsWrapper>
+            <BookDetailsContainer>
+              <BookImage
+                width={171}
+                height={242}
+                alt="Book name"
+                src={"https://github.com/Victtor-777.png"}
+              />
+              <BookContent>
+                <div>
+                  <Heading size={"sm"}>Book Name</Heading>
+                  <Text color={"gray-300"} css={{ marginTop: "$2" }}>
+                    Joe Doe
+                  </Text>
+                </div>
+
+                <div>
+                  <RatingStars rating={4} size={"md"} />
+                  <Text
+                    color={"gray-400"}
+                    size={"sm"}
+                    css={{ marginTop: "$1" }}
+                  >2 avaliações</Text>
+                </div>
+              </BookContent>
+            </BookDetailsContainer>
+            <BookInfos>
+              <BookInfo icon={<BookmarkSimple />} title="Categorias" info="Ficção, Ação" />
+              <BookInfo icon={<BookOpen />} title="Páginas" info="217" />
+            </BookInfos>
+          </BookDetailsWrapper>
         </DialogContent>
       </Dialog.Portal>
     </Dialog.Root>
-  )
-}
+  );
+};
