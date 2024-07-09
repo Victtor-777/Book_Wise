9b70e2bc7580336344b179b03e72b8b8e829af09
Victtor777 <victtor.eng.software@gmail.com>
2024-07-09 18:04:31 -0300

    Add component que mostra o ultimo livro avaliada e finalização do projeto

--- src/components/LatestRatings/index.tsx
+++ src/components/LatestRatings/index.tsx
@@ -1,10 +1,12 @@
 import { ChartLineUp } from "@phosphor-icons/react";
 import { PageTitle } from "../ui/PageTitle";
-import { Container } from "./styles";
+import { Container, LatestContainer } from "./styles";
 import { Text } from "../Typography";
 import { RatingCard, RatingWithAuthorAndBook } from "../RatingCard";
 import { useQuery } from "@tanstack/react-query";
 import { api } from "@/lib/axios";
+import { useSession } from "next-auth/react";
+import { Link } from "../ui/Link";
 
 export const LatestRatings = () => {
   const { data: ratings } = useQuery<RatingWithAuthorAndBook[]>({
@@ -13,6 +15,19 @@ export const LatestRatings = () => {
       const { data } = await api.get("/ratings/latest");
       return data?.ratings ?? [];
     },
+  });
+
+  const { data: session } = useSession();
+
+  const userId = session?.user.id;
+
+  const { data: latestUserRating } = useQuery<RatingWithAuthorAndBook>({
+    queryKey: ["latest-user-rating", userId],
+    queryFn: async () => {
+      const { data } = await api.get("/ratings/user-latest");
+      return data.rating ?? null;
+    },
+    enabled: !!userId,
   });
 
   return (
@@ -24,6 +39,17 @@ export const LatestRatings = () => {
       />
 
       <Text size={"sm"}>Avaliações mais recentes</Text>
+
+      {latestUserRating && (
+        <LatestContainer>
+          <header>
+            <Text size={"sm"}>Sua última avaliação</Text>
+            <Link text="Ver todas" href={`/profile/${userId}`} />
+          </header>
+
+          <RatingCard variant="compact" rating={latestUserRating} />
+        </LatestContainer>
+      )}
 
       <section>
         {ratings?.map((rating) => (
