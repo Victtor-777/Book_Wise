768b7be41f043b8bfd1bceffc2aefb8c636d6f26
Victtor777 <victtor.eng.software@gmail.com>
2024-07-09 17:23:21 -0300

    Página de perfil finalizada

--- src/pages/profile/[id].tsx
+++ src/pages/profile/[id].tsx
@@ -7,6 +7,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import { api } from "@/lib/axios";
 import { useSession } from "next-auth/react";
+import { ProfileDetails } from "@/components/ProfileDetails";
 
 export type ProfileData = {
   ratings: ProfileRating[];
@@ -48,6 +49,7 @@ const ProfilePage: NextPageWithLayout = () => {
             isOwnProfile={isOwnProfile}
             ratings={profile?.ratings}
           />
+          <ProfileDetails profile={profile} />
         </>
       ) : (
         <h1>Carregando...</h1>
